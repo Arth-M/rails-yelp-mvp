c936d02ea1f6f68572985c1bffae4d05962d35e3
Arthur-Henri Michalland <ah.michalland@gmail.com>
2024-08-07 15:57:03 +0200

    made restaurant app, ugly but functionnal

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#
+#
+puts 'Destroy all restaurants and reviews'
+Restaurant.destroy_all
+puts 'Restaurants and reviews destroyed'
+
+puts 'creating new restaurants'
+Restaurant.create!(name: "Chez boris", address:"Montpel", category:"french")
+Restaurant.create!(name:"Bi bim bap", address:"Paris", category:"chinese")
+Restaurant.create!(name:"Grand slam burger", address:"Montpel", category:"french")
+Restaurant.create!(name:"Pizzas", address:"Toulouse", category:"italian")
+Restaurant.create!(name:"La moule", address:"Bordeaux", category:"belgian")
+Restaurant.create!(name:"Tsuki suchi", address:"Clermont-Ferrand", category: "japanese")
+puts 'Created new restaurants'
